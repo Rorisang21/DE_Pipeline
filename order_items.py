013320c05735452102844727cdd710d4a698f34f
Rory <110104063+Rorisang21@users.noreply.github.com>
2026-03-26 00:17:59 +0200

    Delete order_items.py

--- order_items.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-order_items_df= pd.read_csv(r"C:\Users\tatty\OneDrive\Desktop\DATA ENGINEERING PROJECT\DATA\order_items.csv")
-order_items_df = order_items_df[
-    (order_items_df['quantity'] > 0) &
-    (order_items_df['unit_price'] > 0)
-]
-order_items_df = order_items_df.reset_index(drop=True)
-
-print(order_items_df)
